0f30ba1c34092d39d58e5f72dcd531282d457c9f
sujrhung <58470172+sujrhung@users.noreply.github.com>
2019-12-10 21:47:29 +0800

    Update AIp12c.py

--- AIp12c.py
+++ AIp12c.py
@@ -61,9 +61,9 @@ app = Flask(__name__)  # __name__ 代表目前執行的模組
 
 ###=== (5.3) LINE介面密碼 ===### (參考3.3)
 ##== (1) Channel Access Token
-line_bot_api = LineBotApi("Nr6mCIsNGslpKJagqwGXYLetpQx0UF2bfmvDAupvFIMmZ/ntSDWrVcRAPOI+OUeklrEWYaU96foNY0rOD+4wXNNPkvAKVGdFbXkcu3r9fblG+zBFT7dx4wjhXksPINOC4G3q6XffuRn/WDIJXoXNEQdB04t89/1O/w1cDnyilFU=")  #-- YOUR_CHANNEL_ACCESS_TOKEN
+line_bot_api = LineBotApi("A8Swb9UQW020Hc99fEsOj376iPRx+gbYHjoX9Ynjc60tqgHsEdhBkrUfCuIRg0yPxDRdw69kAfDO8RCZUhvx9bzBgfjHI9RXmrUINgCFVne9w4j6C4BVnx6AaYvS8lyZPflh5R/CTTQ8PlmSuB7CSwdB04t89/1O/w1cDnyilFU=")  #-- YOUR_CHANNEL_ACCESS_TOKEN
 ##== (2) Channel Secret
-handler = WebhookHandler("48f6b1096e13a1d04269785c75363a8c")  #-- YOUR_CHANNEL_SECRET
+handler = WebhookHandler("2bddb49336a4c08f6a6d371f63936dae")  #-- YOUR_CHANNEL_SECRET
 
 ###=== (5.4) 監聽來自 /callback 的 Post Request  ===###
 @app.route("/callback", methods=['POST']) 
